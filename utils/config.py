84b5859b4e677b87803e3a8a8d0e2f778a20c245
Sara-Rajaee <11276587+Sara-Rajaee@users.noreply.github.com>
2023-01-27 16:07:49 +0100

    config updated.

--- utils/config.py
+++ utils/config.py
@@ -4,8 +4,8 @@ from dataclasses import dataclass
 @dataclass
 class DataConfig:
     data_dir: str
-    valid_labels: str
     valid_rep: str
+    valid_labels: str = None
     train_labels: str = None
     train_rep: str = None
 
@@ -39,16 +39,22 @@ data_configs = {
     ),
     "projecting_rep_el": DataConfig(
         data_dir="/ivi/ilps/projects/ltl-mt/probings/",
-        train_labels="mnli_valid_greek_labels.npy",
         train_rep="mnli_valid_greek_rep.npy",
-        valid_labels="mnli_valid_greek_labels.npy",
         valid_rep="mnli_valid_greek_rep.npy",
     ),
     "projecting_rep_en": DataConfig(
         data_dir="/ivi/ilps/projects/ltl-mt/probings/",
-        train_labels="mnli_training_enligh_labels.npy",
         train_rep="mnli_training_enligh_rep.npy",
-        valid_labels="mnli_valid_enligh_labels.npy",
         valid_rep="mnli_valid_enligh_rep.npy",
     ),
+}
+
+
+@dataclass
+class ModelConfig:
+    probe_dir: str
+
+
+model_configs ={
+    "cordInv": ModelConfig(probe_dir='models/cordInv_valid_enligh_labels.npy')
 }
