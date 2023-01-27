52398e438c9c73b3327f6e8206add9415df1dded
Sergey Troshin <stroshi@ilps-cn001.ivi-ilps.local>
2023-01-27 17:07:58 +0100

    fix projection

--- utils/projection.py
+++ utils/projection.py
@@ -1,20 +1,39 @@
 import torch
 
+
 def stretch_along(emb, direction, gamma=1.0):
     # emb: (N, d)
     # direction: (M, d)
     # gamma: float, torch.tensor
-    # returns: (N, M, d) matrix, emb + gamma * dot(emb, direction) * direction
+    # returns: (N, d) matrix, emb + gamma * dot(emb, direction) * direction
     direction = direction / torch.norm(direction, dim=-1, keepdim=True)
-    return  emb.unsqueeze(1) + gamma * torch.sum(emb[:, None, :] * direction[None, :, :], dim=-1, keepdims=True) * direction[None, :, :]
+    return emb + torch.sum(
+        torch.sum(emb[:, None, :] * direction[None, :, :], dim=-1, keepdims=True)
+        * direction[None, :, :]
+        * gamma,
+        dim=1,
+    )
+
 
 def main():
     emb = torch.tensor([[0, 0, 1], [0, 1, 0], [1, 0, 0]], dtype=torch.float)
-    direction = torch.tensor([[1, 1, 1]], dtype=torch.float)
+    direction = torch.tensor([[1, 0, 0], [0, 1, 0]], dtype=torch.float)
     print(stretch_along(emb, direction))
     print(stretch_along(emb, direction, gamma=2.0))
+    """Output:
+    tensor([[0., 0., 1.],
+            [0., 2., 0.],
+            [2., 0., 0.]])
+    tensor([[0., 0., 1.],
+            [0., 3., 0.],
+            [3., 0., 0.]])
+    """
+    emb = torch.tensor([[1, 1, 1]], dtype=torch.float)
     direction = torch.tensor([[1, 0.0, 0.0], [0.0, 1, 0.0]], dtype=torch.float)
     print(stretch_along(emb, direction, gamma=1.0))
+    """Output:
+    tensor([[2., 2., 1.]])
+    """ 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
